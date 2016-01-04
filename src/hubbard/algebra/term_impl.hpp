3ac9df2dbe1b91b71eda8a25bf076af0e2cbb946
Fabian Köhler <fkoehler1024@googlemail.com>
2016-01-04 14:08:46 +0100

    add test for order_term

--- src/hubbard/algebra/term_impl.hpp
+++ src/hubbard/algebra/term_impl.hpp
@@ -1,4 +1,5 @@
 #include "hubbard/algebra/term.hpp"
+#include "hubbard/algebra/commutator.hpp"
 
 namespace hubbard
 {
--- tests/algebra/test_term.cpp
+++ tests/algebra/test_term.cpp
@@ -44,3 +44,46 @@ TEST_CASE("find_unordered_operator", "[algebra]")
                      {make_creator(0, true), make_annihilator(0, true), make_creator(0, true)});
     REQUIRE(find_unordered_operator(term) == term.operators.begin() + 1);
 }
+
+TEST_CASE("order_term", "[algebra]")
+{
+    auto factor = Complex(1., 0.);
+    auto term   = make_term(factor, {make_creator(0, true), make_annihilator(0, true)});
+
+    TermList<Term<Operator<int, bool>, Complex>> list;
+    list.push_back(term);
+    order_term(list.front(), list);
+    REQUIRE(list.size() == 1);
+    REQUIRE(list.front() == term);
+
+    list.clear();
+    term = make_term(factor, {make_annihilator(0, true), make_creator(0, true)});
+    list.push_back(term);
+    order_term(list.front(), list);
+    REQUIRE(list.size() == 2);
+    REQUIRE(list[0] == make_term(-factor, {make_creator(0, true), make_annihilator(0, true)}));
+    REQUIRE(list[1] == make_term(factor, std::initializer_list<Operator<int, bool>>()));
+
+    list.clear();
+    term = make_term(factor,
+                     {make_creator(0, true), make_annihilator(0, true), make_creator(0, true)});
+    list.push_back(term);
+    order_term(list.front(), list);
+    REQUIRE(list.size() == 2);
+    REQUIRE(list[0] == make_term(-factor, {make_creator(0, true), make_creator(0, true),
+                                           make_annihilator(0, true)}));
+    REQUIRE(list[1] == make_term(factor, {make_creator(0, true)}));
+
+    list.clear();
+    term =
+        make_term(factor, {make_creator(0, true), make_annihilator(0, true), make_creator(1, true),
+                           make_annihilator(1, true), make_creator(0, true)});
+    list.push_back(term);
+    order_term(list.front(), list);
+    REQUIRE(list.size() == 2);
+    REQUIRE(list[0] == make_term(-factor, {make_creator(0, true), make_creator(1, true),
+                                           make_creator(0, true), make_annihilator(0, true),
+                                           make_annihilator(1, true)}));
+    REQUIRE(list[1] == make_term(factor, {make_creator(0, true), make_creator(1, true),
+                                          make_annihilator(1, true)}));
+}
