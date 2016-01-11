b3bf959ac9e95ae76b6c0568593c8413730f621c
Fabian Köhler <fkoehler1024@googlemail.com>
2016-01-11 14:12:01 +0100

    use version string from git describe

--- src/regexes.cpp
+++ src/regexes.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
+#include "hubbard/config.hpp"
 #include "hubbard/abstract/parse.hpp"
 
 using namespace std;
 using namespace hubbard::abstract::parse;
 
 int main() {
+    cout << "version:      \t" << hubbard::version << endl;
     cout << "re_commutator:\t" << components::str_commutator << endl;
     cout << "re_operator:  \t" << components::str_operator << endl;
     cout << "re_kronecker: \t" << components::str_kronecker << endl;
     cout << "re_number:    \t" << components::str_number << endl;
-    cout << std::get<0>(hubbard::abstract::parse::is_commutator("--help")) << endl;
 }
