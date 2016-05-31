bdd967ec9cf98ce1311900010b1108e05c68d794
Fabian Köhler <fkoehler1024@googlemail.com>
2016-05-31 23:05:03 +0200

    add exception.hpp

---
+++ include/ieompp/exception.hpp
@@ -0,0 +1,39 @@
+#ifndef IEOMPP_EXCEPTION_HPP_
+#define IEOMPP_EXCEPTION_HPP_
+
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace ieompp
+{
+    class Exception : public std::exception
+    {
+        private:
+            const std::string _msg;
+
+        public:
+            Exception(const std::string& msg) : _msg(msg) {}
+            Exception(const char* msg) : _msg(msg) {}
+            const char* what() const noexcept override { return _msg.c_str(); }
+    };
+
+    class NotImplemented : public Exception
+    {
+        public:
+            NotImplemented(const std::string& msg) : Exception(msg) {}
+            NotImplemented(const char* msg) : Exception(msg) {}
+    };
+
+    template <typename T>
+    T create_exception(const std::string& file, std::size_t line, const std::string& msg)
+    {
+        std::ostringstream strm;
+        strm << file << ':' << line << ": " << msg;
+        return T(strm.str());
+    }
+}
+
+#define THROW(type, msg) throw ieompp::create_exception<type>(__FILE__, __LINE__, msg);
+
+#endif
