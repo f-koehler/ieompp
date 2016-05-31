f8ba25aa789e4d8050b498fa9fcdaf3b1a78d029
Fabian Köhler <fkoehler1024@googlemail.com>
2016-05-31 23:27:50 +0200

    implement commutator generation for 1-operator case

--- include/ieompp/hubbard.hpp
+++ include/ieompp/hubbard.hpp
@@ -2,24 +2,71 @@
 #define HUBBARD_HPP_
 
 #include <cassert>
+#include <cmath>
+#include <type_traits>
 #include <vector>
 
 #include "ieompp/constraints.hpp"
-#include "ieompp/symbolic_index.hpp"
+#include "ieompp/dot_product.hpp"
+#include "ieompp/exception.hpp"
 
 namespace ieompp
 {
     namespace momentum_space
     {
-        template <typename TermT>
-        struct Hamiltonian
-        {
-            using Term = TermT;
-            using Prefactor = typename Term::Prefactor;
-            using Real = typename Prefactor::value_type;
+        template <typename PrefactorT>
+        struct Hamiltonian {
+            using Prefactor = PrefactorT;
 
-            Real J;
-            Real U;
+            Prefactor J, U;
+
+            template <typename Term, typename MomentumSpace, typename Lattice, typename Container>
+            void generate_kinetic_terms(const Term& t, const MomentumSpace& space,
+                                        const Lattice& lattice, Container& container)
+            {
+                static_assert(Term::Operator::number_of_indices == 2,
+                              "Hubbard model operators have two indices!");
+                static_assert(
+                    std::is_same<typename MomentumSpace::Vector, typename Lattice::Vector>::value,
+                    "Momentum space and lattice should have the same type for their vectors!");
+                if(t.operators.size() == 1)
+                    generate_kinetic_terms_1(t, space, lattice, container);
+                else if(t.operators.size() == 3)
+                    generate_kinetic_terms_3(t, space, lattice, container);
+                else
+                    THROW(NotImplemented, "Currently only implemented for 1- and 3-operator terms");
+            }
+
+            template <typename Term, typename MomentumSpace, typename Lattice, typename Container>
+            void generate_kinetic_terms_1(const Term& t, const MomentumSpace& space,
+                                          const Lattice& lattice, Container& container)
+            {
+                if(t.operators.front().creator) {
+                    auto momentum = space[t.operators.front().index1];
+
+                    // TODO: check for type conversion
+                    Prefactor p = 0.;
+                    for(auto& lattice_vector : lattice.lattice_vectors())
+                        p += std::cos(dot_product(momentum, lattice_vector));
+                    p *= -2 * J;
+
+                    Term&& new_term = t;
+                    new_term.prefactpr *= p;
+                    container.emplace_back(new_term);
+                } else {
+                    auto momentum = space[t.operators.front().index1];
+
+                    // TODO: check for type conversion
+                    Prefactor p = 0.;
+                    for(auto& lattice_vector : lattice.lattice_vectors())
+                        p += std::cos(dot_product(momentum, lattice_vector));
+                    p *= +2 * J;
+
+                    Term&& new_term = t;
+                    new_term.prefactpr *= p;
+                    container.emplace_back(new_term);
+                }
+            }
         };
     }
 }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 #include "ieompp/anticommutator.hpp"
 #include "ieompp/symbolic/index.hpp"
+#include "ieompp/hubbard.hpp"
 
 int main()
 {
+    ieompp::momentum_space::Hamiltonian<double> H;
 }
