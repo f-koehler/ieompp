9a5752326a0c169e616be1da36a7f48590d457f7
Fabian Köhler <fkoehler1024@googlemail.com>
2016-06-22 01:40:19 +0200

    fix boost on travis

---
+++ include/ieompp/normalization.hpp
@@ -0,0 +1,12 @@
+#ifndef IEOMPP_NORMALIZATION_HPP_
+#define IEOMPP_NORMALIZATION_HPP_
+
+namespace ieompp
+{
+    template <typename Term, typename Container>
+    void particle_number_form(const Term& t, Container& c)
+    {
+    }
+}
+
+#endif
