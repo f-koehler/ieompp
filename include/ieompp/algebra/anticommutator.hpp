d28f9297cdea461833908c03a9906e111d78e418
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-08 16:37:14 +0200

    make methods of intermediate objects in anticommutator static

--- include/ieompp/algebra/anticommutator.hpp
+++ include/ieompp/algebra/anticommutator.hpp
@@ -18,18 +18,18 @@ namespace ieompp
         {
             template <typename Operator, std::size_t I, std::size_t N>
             struct anticommutates_helper {
-                bool operator()(const Operator& a, const Operator& b) const
+                static bool call(const Operator& a, const Operator& b)
                 {
-                    static const anticommutates_helper<Operator, I + 1, N> helper;
-                    if(is_symbolic_index<I, Operator>::value) return helper(a, b);
+                    if(is_symbolic_index<I, Operator>::value)
+                        return anticommutates_helper<Operator, I + 1, N>::call(a, b);
                     if(get_index<I>(a) != get_index<I>(b)) return true;
-                    return helper(a, b);
+                    anticommutates_helper<Operator, I + 1, N>::call(a, b);
                 }
             };
 
             template <typename Operator, std::size_t N>
             struct anticommutates_helper<Operator, N, N> {
-                bool operator()(const Operator& a, const Operator& b) const
+                static bool call(const Operator& a, const Operator& b)
                 {
                     if(is_symbolic_index<N, Operator>::value) return false;
                     return (get_index<N>(a) != get_index<N>(b));
@@ -38,23 +38,20 @@ namespace ieompp
 
             template <typename Operator, typename Prefactor, std::size_t I, std::size_t N>
             struct anticommutator_helper {
-                void operator()(const Operator& op1, const Operator& op2,
-                                Prefactor& prefactor) const
+                static void call(const Operator& op1, const Operator& op2, Prefactor& prefactor)
                 {
-                    static const anticommutator_helper<Operator, Prefactor, I + 1, N> helper;
                     if(is_symbolic_index<I, Operator>::value) {
                         auto& idx1 = get_index<I>(op1);
                         auto& idx2 = get_index<I>(op2);
                         if(idx1 != idx2) prefactor *= make_kronecker(idx1, idx2);
                     }
-                    helper(op1, op2, prefactor);
+                    anticommutator_helper<Operator, Prefactor, I + 1, N>::call(op1, op2, prefactor);
                 }
             };
 
             template <typename Operator, typename Prefactor, std::size_t N>
             struct anticommutator_helper<Operator, Prefactor, N, N> {
-                void operator()(const Operator& op1, const Operator& op2,
-                                Prefactor& prefactor) const
+                static void call(const Operator& op1, const Operator& op2, Prefactor& prefactor)
                 {
                     if(is_symbolic_index<N, Operator>::value) {
                         auto& idx1 = get_index<N>(op1);
@@ -76,9 +73,9 @@ namespace ieompp
         typename std::enable_if<is_symbolic_operator<Operator>::value, bool>::type
         anticommutates(const Operator& op1, const Operator& op2)
         {
-            static const detail::anticommutates_helper<Operator, 0, Operator::number_of_indices - 1>
-                helper;
-            return (op1.creator == op2.creator) || helper(op1, op2);
+            return (op1.creator == op2.creator)
+                   || detail::anticommutates_helper<Operator, 0, Operator::number_of_indices
+                                                                     - 1>::call(op1, op2);
         }
 
         template <typename Term>
@@ -86,11 +83,9 @@ namespace ieompp
         anticommutator(const typename Term::Operator& op1, const typename Term::Operator& op2,
                        typename Term::Prefactor& prefactor)
         {
-            static const detail::anticommutator_helper<typename Term::Operator,
-                                                       typename Term::Prefactor, 0,
-                                                       Term::Operator::number_of_indices - 1>
-                helper;
-            helper(op1, op2, prefactor);
+            detail::anticommutator_helper<typename Term::Operator, typename Term::Prefactor, 0,
+                                          Term::Operator::number_of_indices - 1>::call(op1, op2,
+                                                                                       prefactor);
         }
 
         template <typename Term>
