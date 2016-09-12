3f71d79425b1accfb413cc925b3f71ced3095e9a
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-12 23:12:21 +0200

    enable more warnings for Clang

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,5 @@ using namespace std;
 
 int main()
 {
-    cout << (1.e-308 == 0.) << '\n';
-    cout << ieompp::types::is_zero(1.e-308) << '\n';
-    cout << std::numeric_limits<double>::min() << '\n';
     return 0;
 }
