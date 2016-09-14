c3b9e0a54ea26dcb9c6209103810b442fccc3f37
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-14 13:07:04 +0200

    restructure platform.hpp, silence some unnecessary warnings

---
+++ tests/test.hpp
@@ -0,0 +1,11 @@
+#ifndef TEST_HPP_
+#define TEST_HPP_
+
+#include <boost/predef.h>
+#if(BOOST_COMP_CLANG)
+#if __has_warning("-Wdisabled-macro-expansion")
+#pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
+#endif
+#endif
+
+#endif
