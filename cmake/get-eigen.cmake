dd92ca048595d80dddb5813f7f23ef38d0fb6585
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-14 12:33:37 +0200

    make targets depend on Eigen project to fix multi-job builds

--- cmake/get-eigen.cmake
+++ cmake/get-eigen.cmake
@@ -1,7 +1,7 @@
 function(get_eigen GET_EIGEN_SOURCE_PATH)
     include(ExternalProject)
     ExternalProject_Add(
-        External_Eigen
+        EigenExternal
         SOURCE_DIR ${GET_EIGEN_SOURCE_PATH}
         #URL https://bitbucket.org/eigen/eigen/get/3.2.9.tar.gz
         #URL_HASH SHA512=bf669ec777ce4756ad6033e21bcf5d490cd3488159913fc24a08019fc59aed3e2b55a5732cfd66d88d72eedf939b26f71366d5ade1e9c6ed6bf765da5639c3fa
@@ -12,4 +12,8 @@ function(get_eigen GET_EIGEN_SOURCE_PATH)
         BUILD_COMMAND     ""
         INSTALL_COMMAND   ""
     )
+endfunction()
+
+function(depend_eigen TARGET)
+    add_dependencies(${TARGET} EigenExternal)
 endfunction()
