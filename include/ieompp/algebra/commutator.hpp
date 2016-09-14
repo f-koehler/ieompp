5e529c110294a43fa6c9e25822446305a28d20f5
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-14 13:28:14 +0200

    clearify where conversions from signed to unsigned occur

--- include/ieompp/algebra/commutator.hpp
+++ include/ieompp/algebra/commutator.hpp
@@ -34,6 +34,9 @@ namespace ieompp
 
             assert(m * n % 2 == 0);
 
+            using iter_diff_type =
+                typename std::vector<typename Term::Operator>::const_iterator::difference_type;
+
             const auto prefactor = a.prefactor * b.prefactor;
             for(std::size_t k = 1; k <= n; ++k) {
                 for(std::size_t l = 1; l <= m; ++l) {
@@ -44,19 +47,19 @@ namespace ieompp
                     Term&& new_term    = Term();
                     new_term.prefactor = prefactor;
                     new_term.prefactor *= sign;
-                    /* if(is_symbolic_operator<typename Term::Operator>::value) */
-                    /*     new_term.prefactor *= anticommutator(a_ops[k - 1], b_ops[l - 1]); */
                     anticommutator<Term>(a_ops[k - 1], b_ops[l - 1], new_term.prefactor);
 
                     auto& new_ops = new_term.operators;
                     if(k > 1)
-                        std::copy(a_ops.begin(), a_ops.begin() + (k - 1),
+                        std::copy(a_ops.begin(), a_ops.begin() + static_cast<iter_diff_type>(k - 1),
                                   std::back_inserter(new_ops));
                     if(l > 1)
-                        std::copy(b_ops.begin(), b_ops.begin() + (l - 1),
+                        std::copy(b_ops.begin(), b_ops.begin() + static_cast<iter_diff_type>(l - 1),
                                   std::back_inserter(new_ops));
-                    std::copy(b_ops.begin() + l, b_ops.end(), std::back_inserter(new_ops));
-                    std::copy(a_ops.begin() + k, a_ops.end(), std::back_inserter(new_ops));
+                    std::copy(b_ops.begin() + static_cast<iter_diff_type>(l), b_ops.end(),
+                              std::back_inserter(new_ops));
+                    std::copy(a_ops.begin() + static_cast<iter_diff_type>(k), a_ops.end(),
+                              std::back_inserter(new_ops));
 
                     c.emplace_back(new_term);
                 }
