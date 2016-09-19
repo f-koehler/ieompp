af5975e491601fcd57c1bb8c53095aba12de70a9
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-19 13:09:24 +0200

    implement matrix elements of hopping

---
+++ include/ieompp/models/hubbard_explicit/operator.hpp
@@ -0,0 +1,19 @@
+#ifndef IEOMPP_MODELS_HUBBARD_OPERATOR_HPP_
+#define IEOMPP_MODELS_HUBBARD_OPERATOR_HPP_
+
+#include <type_traits>
+
+namespace ieompp
+{
+    namespace hubbard
+    {
+        template <typename Operator>
+        struct is_hubbard_operator {
+            static constexpr bool value = (Operator::number_of_indices == 2)
+                                          && std::is_integral<typename Operator::Index1>::value
+                                          && std::is_same<typename Operator::Index2, bool>::value;
+        };
+    }
+}
+
+#endif
