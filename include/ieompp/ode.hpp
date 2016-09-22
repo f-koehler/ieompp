498e15df59bc0fd7a54909166ea0a022606082e6
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-22 15:04:51 +0200

    very general implementation of RK4 using linear algebra

--- include/ieompp/ode.hpp
+++ include/ieompp/ode.hpp
@@ -1,58 +1,40 @@
 #ifndef IEOMPP_ODE_HPP_
 #define IEOMPP_ODE_HPP_
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#include <Eigen/Dense>
-#pragma GCC diagnostic pop
+#include <ieompp/types/number.hpp>
+#include <ieompp/types/eigen.hpp>
 
 namespace ieompp
 {
-    template <typename AgendaT>
+    template <typename MatrixT, typename VectorT>
     class RungeKutta4
     {
-        using Agenda = AgendaT;
-        using Prefactor = typename Agenda::Prefactor;
-        using Matrix = Eigen::Matrix<Prefactor, Eigen::Dynamic, Eigen::Dynamic>;
-        using Vector = Eigen::Matrix<Prefactor, Eigen::Dynamic, 1>;
+        using Matrix = MatrixT;
+        using Vector = VectorT;
+        using StepWidth =
+            typename types::real_type<typename types::eigen_matrix_traits<Matrix>::Scalar>::type;
+        using Complex = typename types::eigen_matrix_traits<Vector>::Scalar;
 
-        private:
-            Matrix _matrix;
-            Vector _current;
-            Prefactor _dt, _dt2, _dt6;
-            Vector _k1, _k2, _k3, _k4;
+        static_assert(types::is_complex<Complex>::value, "A complex Vector has to be used!");
 
-        public:
-            RungeKutta4(const Agenda& agenda, const Vector& initial, const Prefactor& dt)
-            {
-                const auto N = agenda.terms().size();
-                _matrix      = Matrix::Zero(N, N);
-                _current     = initial;
-                _dt          = dt;
-                _dt2         = _dt / 2.;
-                _dt6         = _dt / 6.;
-                _k1          = Vector::Zero(N);
-                _k2          = Vector::Zero(N);
-                _k3          = Vector::Zero(N);
-                _k4          = Vector::Zero(N);
+      private:
+        const std::size_t _N;
+        Matrix _matrix;
+        Vector _k1, _k2, _k3, _k4;
 
-                for(std::size_t i = 0; i < N; ++i) {
-                    const auto& row = agenda[i];
-                    for(auto& coeff : row)
-                        _matrix(i, coeff.index) += Prefactor(0, 1) * coeff.prefactor;
-                }
-            }
+      public:
+        RungeKutta4(std::size_t N) : _N(N), _k1(N), _k2(N), _k3(N), _k4(N) {}
+        const Matrix& matrix() const { return _matrix; }
+        Matrix& matrix() { return _matrix; }
 
-            void step() {
-                _k1 = _matrix * _current;
-                _k2 = _matrix * (_current + _dt2 * _k1);
-                _k3 = _matrix * (_current + _dt2 * _k2);
-                _k4 = _matrix * (_current + _dt * _k3);
-                _current += _dt6 * (_k1 + 2. * _k2 + 2. * _k3 + _k4);
-            }
-
-            const Matrix& matrix() const { return _matrix; }
-            const Vector& solution() const { return _current; }
+        void step(Vector& current, const StepWidth dt)
+        {
+            _k1 = _matrix * current;
+            _k2 = _matrix * (current + (dt / 2) * _k1);
+            _k3 = _matrix * (current + (dt / 2) * _k2);
+            _k4 = _matrix * (current + dt * _k3);
+            current += (dt * Complex(0., 1.) / 6.) * (_k1 + 2. * _k2 + 2. * _k3 + _k4);
+        }
     };
 }
 
