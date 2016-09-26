d0194114797cb7edeab3badfadc4c7930f35fb5b
Fabian Köhler <fkoehler1024@googlemail.com>
2016-09-27 01:27:21 +0200

    implement parallel matrix initialization for dense matrices

--- include/ieompp/types/eigen_init.hpp
+++ include/ieompp/types/eigen_init.hpp
@@ -12,13 +12,19 @@ namespace ieompp
     namespace types
     {
         template <typename Matrix>
-        typename std::enable_if<is_sparse_eigen_matrix<Matrix>::value, void>::type init(
-            Matrix& m, std::size_t rows, std::size_t cols,
-            std::function<typename eigen_matrix_traits<Matrix>::Scalar(std::size_t, std::size_t)> f)
+        typename std::enable_if<is_sparse_eigen_matrix<Matrix>::value, void>::type
+        init(Matrix& m, typename eigen_matrix_traits<Matrix>::Index rows,
+             typename eigen_matrix_traits<Matrix>::Index cols,
+             std::function<typename eigen_matrix_traits<Matrix>::Scalar(
+                 typename eigen_matrix_traits<Matrix>::Index,
+                 typename eigen_matrix_traits<Matrix>::Index)>
+                 f)
         {
+            using Index = typename eigen_matrix_traits<Matrix>::Index;
+
             m = Matrix(rows, cols);
-            for(std::size_t i = 0; i < rows; ++i) {
-                for(std::size_t j = 0; j < cols; ++j) {
+            for(Index i = 0; i < rows; ++i) {
+                for(Index j = 0; j < cols; ++j) {
                     const auto val = f(i, j);
                     if(!is_zero(val)) m.insert(i, j) = val;
                 }
@@ -26,13 +32,18 @@ namespace ieompp
         }
 
         template <typename Matrix>
-        typename std::enable_if<is_sparse_eigen_matrix<Matrix>::value, void>::type init_symmetric(
-            Matrix& m, std::size_t rows,
-            std::function<typename eigen_matrix_traits<Matrix>::Scalar(std::size_t, std::size_t)> f)
+        typename std::enable_if<is_sparse_eigen_matrix<Matrix>::value, void>::type
+        init_symmetric(Matrix& m, typename eigen_matrix_traits<Matrix>::Index rows,
+                       std::function<typename eigen_matrix_traits<Matrix>::Scalar(
+                           typename eigen_matrix_traits<Matrix>::Index,
+                           typename eigen_matrix_traits<Matrix>::Index)>
+                           f)
         {
+            using Index = typename eigen_matrix_traits<Matrix>::Index;
+
             m = Matrix(rows, rows);
-            for(std::size_t i = 0; i < rows; ++i) {
-                for(std::size_t j = 0; j < i; ++j) {
+            for(Index i = 0; i < rows; ++i) {
+                for(Index j = 0; j < i; ++j) {
                     const auto val = f(i, j);
                     if(!is_zero(val)) {
                         m.insert(i, j) = val;
@@ -45,26 +56,37 @@ namespace ieompp
         }
 
         template <typename Matrix>
-        typename std::enable_if<!is_sparse_eigen_matrix<Matrix>::value, void>::type init(
-            Matrix& m, std::size_t rows, std::size_t cols,
-            std::function<typename eigen_matrix_traits<Matrix>::Scalar(std::size_t, std::size_t)> f)
+        typename std::enable_if<is_dense_eigen_matrix<Matrix>::value, void>::type
+        init(Matrix& m, typename eigen_matrix_traits<Matrix>::Index rows,
+             typename eigen_matrix_traits<Matrix>::Index cols,
+             std::function<typename eigen_matrix_traits<Matrix>::Scalar(
+                 typename eigen_matrix_traits<Matrix>::Index,
+                 typename eigen_matrix_traits<Matrix>::Index)>
+                 f)
         {
+            using Index = typename eigen_matrix_traits<Matrix>::Index;
+
             m = Matrix::Zero(rows, cols);
-            for(std::size_t i = 0; i < rows; ++i) {
-                for(std::size_t j = 0; j < cols; ++j) {
+            for(Index i = 0; i < rows; ++i) {
+                for(Index j = 0; j < cols; ++j) {
                     m(i, j) = f(i, j);
                 }
             }
         }
 
         template <typename Matrix>
-        typename std::enable_if<!is_sparse_eigen_matrix<Matrix>::value, void>::type init_symmetric(
-            Matrix& m, std::size_t rows,
-            std::function<typename eigen_matrix_traits<Matrix>::Scalar(std::size_t, std::size_t)> f)
+        typename std::enable_if<is_dense_eigen_matrix<Matrix>::value, void>::type
+        init_symmetric(Matrix& m, typename eigen_matrix_traits<Matrix>::Index rows,
+                       std::function<typename eigen_matrix_traits<Matrix>::Scalar(
+                           typename eigen_matrix_traits<Matrix>::Index,
+                           typename eigen_matrix_traits<Matrix>::Index)>
+                           f)
         {
+            using Index = typename eigen_matrix_traits<Matrix>::Index;
+
             m = Matrix::Zero(rows, rows);
-            for(std::size_t i = 0; i < rows; ++i) {
-                for(std::size_t j = 0; j < i; ++j) {
+            for(Index i = 0; i < rows; ++i) {
+                for(Index j = 0; j < i; ++j) {
                     const auto val = f(i, j);
                     m(i, j) = val;
                     m(j, i) = val;
@@ -74,37 +96,87 @@ namespace ieompp
         }
 
         template <typename Matrix>
-        typename std::enable_if<is_sparse_eigen_matrix<Matrix>::value, void>::type init_parallel(
-            Matrix& m, std::size_t rows, std::size_t cols,
-            std::function<typename eigen_matrix_traits<Matrix>::Scalar(std::size_t, std::size_t)> f)
+        typename std::enable_if<is_sparse_eigen_matrix<Matrix>::value, void>::type
+        init_parallel(Matrix& m, typename eigen_matrix_traits<Matrix>::Index rows,
+                      typename eigen_matrix_traits<Matrix>::Index cols,
+                      std::function<typename eigen_matrix_traits<Matrix>::Scalar(
+                          typename eigen_matrix_traits<Matrix>::Index,
+                          typename eigen_matrix_traits<Matrix>::Index)>
+                          f)
         {
 #if defined(_OPENMP)
+            using Index = typename eigen_matrix_traits<Matrix>::Index;
             m = Matrix(rows, cols);
 
             omp_lock_t write_lock;
             omp_init_lock(&write_lock);
 
 #pragma omp parallel for
-            for(std::size_t i = 0; i < rows; ++i) {
-                std::vector<typename eigen_matrix_traits<Matrix>::Triplet> triplets;
-                triplets.reserve(1);
-                auto write_back = [&m, &triplets, &write_lock]() {
-                    if(triplets.empty()) return;
-                    omp_set_lock(&write_lock);
-                    m.setFromTriplets(triplets.begin(), triplets.end());
-                    omp_unset_lock(&write_lock);
-                    triplets.clear();
-                };
-                for(std::size_t j = 0; j < cols; ++j) {
-                    triplets.emplace_back(i, j, f(i, j));
-                    if(triplets.size() == triplets.capacity()) write_back();
+            for(Index i = 0; i < rows; ++i) {
+                for(Index j = 0; j < rows; ++j) {
+                    const auto val = f(i, j);
+                    if(!is_zero(val)) {
+                        omp_set_lock(&write_lock);
+                        m.insert(i, j) = val;
+                        omp_unset_lock(&write_lock);
+                    }
                 }
-                write_back();
             }
 
+            /* #pragma omp parallel for */
             omp_destroy_lock(&write_lock);
 #else
             init<Matrix>(rows, cols, f);
+#endif
+        }
+
+        template <typename Matrix>
+        typename std::enable_if<is_dense_eigen_matrix<Matrix>::value, void>::type
+        init_parallel(Matrix& m, typename eigen_matrix_traits<Matrix>::Index rows,
+                      typename eigen_matrix_traits<Matrix>::Index cols,
+                      std::function<typename eigen_matrix_traits<Matrix>::Scalar(
+                          typename eigen_matrix_traits<Matrix>::Index,
+                          typename eigen_matrix_traits<Matrix>::Index)>
+                          f)
+        {
+#if defined(_OPENMP)
+            using Index = typename eigen_matrix_traits<Matrix>::Index;
+
+            m = Matrix::Zero(rows, cols);
+#pragma omp parallel for
+            for(Index i = 0; i < rows; ++i) {
+                for(Index j = 0; j < cols; ++j) {
+                    m(i, j) = f(i, j);
+                }
+            }
+#else
+            init_symmetric(m, rows, cols, f);
+#endif
+        }
+
+        template <typename Matrix>
+        typename std::enable_if<is_dense_eigen_matrix<Matrix>::value, void>::type
+        init_symmetric_parallel(Matrix& m, typename eigen_matrix_traits<Matrix>::Index rows,
+                                std::function<typename eigen_matrix_traits<Matrix>::Scalar(
+                                    typename eigen_matrix_traits<Matrix>::Index,
+                                    typename eigen_matrix_traits<Matrix>::Index)>
+                                    f)
+        {
+#if defined(_OPENMP)
+            using Index = typename eigen_matrix_traits<Matrix>::Index;
+
+            m = Matrix::Zero(rows, rows);
+#pragma omp parallel for
+            for(Index i = 0; i < rows; ++i) {
+                for(Index j = 0; j < i; ++j) {
+                    const auto val = f(i, j);
+                    m(i, j) = val;
+                    m(j, i) = val;
+                }
+                m(i, i) = f(i, i);
+            }
+#else
+            init_symmetric(m, rows, f);
 #endif
         }
     }
