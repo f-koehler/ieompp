0be6557953a08dc04f3cce3f895b87aeccf5db31
Fabian Köhler <fkoehler1024@googlemail.com>
2016-10-23 16:49:13 +0200

    write compile script for CL1

--- tools/compile-cl1.sh
+++ tools/compile-cl1.sh
@@ -1,8 +1,55 @@
 #!/bin/bash
 NICE_LVL=19
+NPROC=6
+COMMON_FLAGS="-pipe -m64 -isystem/net/sw/gcc/5.3.0/rft/include -L/net/sw/gcc/5.3.0/rtf/lib64 -lboost_program_options"
+
+module load gcc/5.3.0-full 
+module load python/3.4.2
+
+# sandy bridge build
+mkdir build-sandybridge
+cd build-sandybridge
+cmake                                                      \
+    -DCMAKE_C_FLAGS="${COMMON_FLAGS} -march=sandybridge"   \
+    -DCMAKE_CXX_FLAGS="${COMMON_FLAGS} -march=sandybridge" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/sandybridge               \
+    -DCMAKE_C_COMPILER=/net/sw/gcc/5.3.0/rtf/bin/gcc       \
+    -DCMAKE_CXX_COMPILER=/net/sw/gcc/5.3.0/rtf/bin/g++     \
+    -DCMAKE_BUILD_TYPE=Release                             \
+    -Dieompp_ENABLE_TESTS=OFF                              \
+    -Dieompp_DISABLE_NATIVE=ON                             \
+    ..
+nice -n 19 make -j${NPROC} install
+cd ..
 
 # haswell build
 mkdir build-haswell
 cd build-haswell
-module load gcc/5.3.0-full 
-module load python/3.4.2
+cmake                                                  \
+    -DCMAKE_C_FLAGS="${COMMON_FLAGS} -march=haswell"   \
+    -DCMAKE_CXX_FLAGS="${COMMON_FLAGS} -march=haswell" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/haswell               \
+    -DCMAKE_C_COMPILER=/net/sw/gcc/5.3.0/rtf/bin/gcc   \
+    -DCMAKE_CXX_COMPILER=/net/sw/gcc/5.3.0/rtf/bin/g++ \
+    -DCMAKE_BUILD_TYPE=Release                         \
+    -Dieompp_ENABLE_TESTS=OFF                          \
+    -Dieompp_DISABLE_NATIVE=ON                         \
+    ..
+nice -n 19 make -j${NPROC} install
+cd ..
+
+# broadwell build
+mkdir build-broadwell
+cd build-broadwell
+cmake                                                    \
+    -DCMAKE_C_FLAGS="${COMMON_FLAGS} -march=broadwell"   \
+    -DCMAKE_CXX_FLAGS="${COMMON_FLAGS} -march=broadwell" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/broadwell               \
+    -DCMAKE_C_COMPILER=/net/sw/gcc/5.3.0/rtf/bin/gcc     \
+    -DCMAKE_CXX_COMPILER=/net/sw/gcc/5.3.0/rtf/bin/g++   \
+    -DCMAKE_BUILD_TYPE=Release                           \
+    -Dieompp_ENABLE_TESTS=OFF                            \
+    -Dieompp_DISABLE_NATIVE=ON                           \
+    ..
+nice -n 19 make -j${NPROC} install
+cd ..
