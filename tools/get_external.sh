ce51324bb51b1e9da3048879c2c0d7e411945253
Fabian Köhler <fkoehler1024@googlemail.com>
2016-10-26 21:35:05 +0200

    update external

--- tools/get_external.sh
+++ tools/get_external.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 EIGEN_SHA512="d494995d5e5a264147141e84fda754950dcb41c6eff94ef3bf14411d8e3eaad4376bc3f634a5edcaa5f13c8be1d845784071f5b181e267c8fad9982fdb1a2d37"
+VERSION="3.2.10"
 
 git submodule update --init --recursive
 RETVAL=$?
@@ -10,7 +11,7 @@ if [ $RETVAL -ne 0 ]; then
 fi
 
 if [ ! -e external/eigen.tar.gz ]; then
-    curl -o external/eigen.tar.gz "https://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz"
+    curl -o external/eigen.tar.gz "https://bitbucket.org/eigen/eigen/get/$VERSION.tar.gz"
     RETVAL=$?
     if [ $RETVAL -ne 0 ]; then
         echo "Failed to download eigen" 1>&2
