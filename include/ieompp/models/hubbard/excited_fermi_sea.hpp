e48470ec7b74ee6a1d87fd1494ccfc6f010531b9
Fabian Köhler <fkoehler1024@googlemail.com>
2016-11-10 15:35:23 +0100

    add excited_fermi_sea.hpp

---
+++ include/ieompp/models/hubbard/excited_fermi_sea.hpp
@@ -0,0 +1,108 @@
+#ifndef IEOMPP_MODELS_HUBBARD_EXCITED_FERMI_SEA_HPP_
+#define IEOMPP_MODELS_HUBBARD_EXCITED_FERMI_SEA_HPP_
+
+#include <algorithm>
+#include <list>
+#include <utility>
+#include <vector>
+
+namespace ieompp
+{
+    namespace hubbard
+    {
+        namespace momentum_space
+        {
+            template <typename IndexT>
+            struct ExcitedFermiSea {
+                using Index = IndexT;
+
+                bool vanishes = false;
+                std::list<Index> created_particles;
+                std::list<Index> annihilated_particles;
+
+                template <typename Term, typename Dispersion>
+                ExcitedFermiSea(const Term& term, const Dispersion& dispersion,
+                                const typename Dispersion::Float& fermi_energy = 0.)
+                {
+                    const auto& ops = term.operators;
+
+                    for(auto it = ops.crbegin(); it != ops.crend(); ++it) {
+                        // get energy of next creation/annihilation
+                        const auto energy = dispersion[it->index1];
+
+                        if(it->creator) {
+                            // check if creation is below fermi level
+                            if(energy < fermi_energy) {
+                                // particle already exists if no annihilation took place
+                                auto find = find_annihilation(it->index1);
+                                if(!find.first) {
+                                    // cannot create another particle
+                                    vanishes = true;
+                                    break;
+                                } else {
+                                    // creation reverts previous annihilation
+                                    annihilated_particles.remove(find.second);
+                                }
+                            } else {
+                                // particle does not exist unless creation took place
+                                auto find = find_creation(it->index1);
+                                if(find.first) {
+                                    // cannot create another particle
+                                    vanishes = true;
+                                    break;
+                                } else {
+                                    // create new particle
+                                    created_particles.push_back(it->index1);
+                                }
+                            }
+                        } else {
+                            // check if annihilation is below fermi level
+                            if(energy < fermi_energy) {
+                                // particle does exist unless annihilation took place
+                                auto find = find_annihilation(it->index1);
+                                if(find.first) {
+                                    // annihilation of annihilated particle fails
+                                    vanishes = true;
+                                    break;
+                                } else {
+                                    // annihilate present particle
+                                    annihilated_particles.push_back(it->index2);
+                                }
+                            } else {
+                                // particle does not exist if no creation took place
+                                auto find = find_creation(it->index1);
+                                if(!find.first) {
+                                    // annihilation of absent particle fails
+                                    vanishes = true;
+                                    break;
+                                } else {
+                                    // annihilate created particle
+                                    created_particles.remove(find.second);
+                                }
+                            }
+                        }
+                    }
+
+                    if(!vanishes) {
+                        std::sort(created_particles.begin(), created_particles.end());
+                        std::sort(annihilated_particles.begin(), annihilated_particles.end());
+                    }
+                }
+
+                auto find_creation(const Index& site)
+                {
+                    auto it = std::find(created_particles.begin(), created_particles.end(), site);
+                    return std::make_pair(it == created_particles.end(), it);
+                }
+
+                auto find_annihilation(const Index& site)
+                {
+                    auto it = std::find(created_particles.begin(), created_particles.end(), site);
+                    return std::make_pair(it == created_particles.end(), it);
+                }
+            };
+        }
+    }
+}
+
+#endif
