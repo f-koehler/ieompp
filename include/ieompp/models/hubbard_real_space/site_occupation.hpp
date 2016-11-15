86c63392b548c149caed1c65003489842f65b12f
Fabian Köhler <fkoehler1024@googlemail.com>
2016-11-15 18:15:23 +0100

    split evaluation of SiteOccupation into several methods, is still buggy

--- include/ieompp/models/hubbard_real_space/site_occupation.hpp
+++ include/ieompp/models/hubbard_real_space/site_occupation.hpp
@@ -21,15 +21,24 @@ namespace ieompp
             struct SiteOccupation {
             };
 
-            template <typename Term>
-            struct SiteOccupation<Basis1Operator<Term>> {
-                using Basis    = Basis1Operator<Term>;
+            template <typename TermT>
+            struct SiteOccupation<Basis1Operator<TermT>> {
+                using Basis    = Basis1Operator<TermT>;
+                using Term     = TermT;
                 using Float    = typename types::RealType<typename Term::Prefactor>::Type;
                 using Operator = typename Term::Operator;
                 using ExpectationValueFunction =
                     std::function<Float(const Operator&, const Operator&)>;
 
                 ExpectationValueFunction expectation_value;
+
+                Float expectation_value_1_1(const Term& a, const Term& b) const
+                {
+                    assert(a.operators.size() == 1);
+                    assert(b.operators.size() == 1);
+
+                    return 2. * expectation_value(a.operators.front(), b.operators.front());
+                }
 
                 template <typename Vector>
                 Float operator()(const Basis& basis, const Vector& vector) const
@@ -41,14 +50,12 @@ namespace ieompp
                     for(auto i = 0ul; i < size; ++i) {
                         const auto thread = omp_get_thread_num();
                         for(auto j = 0ul; j < i; ++j) {
-                            results[thread] +=
-                                expectation_value(basis[i].operators.front(),
-                                                  basis[j].operators.front())
-                                * types::add_conjugate_products(vector[i], vector[j]);
+                            results[thread] += expectation_value_1_1(basis[i], basis[j])
+                                               * types::add_conjugate_products(vector[i], vector[j])
+                                               / 2.;
                         }
                         results[thread] +=
-                            std::norm(vector[i]) * expectation_value(basis[i].operators.front(),
-                                                                     basis[i].operators.front());
+                            std::norm(vector[i]) * expectation_value_1_1(basis[i], basis[i]) / 2.;
                     }
 
                     return std::accumulate(results.begin(), results.end(), 0.);
@@ -65,6 +72,62 @@ namespace ieompp
 
                 ExpectationValueFunction expectation_value;
 
+                Float expectation_value_1_1(const Term& a, const Term& b) const
+                {
+                    assert(a.operators.size() == 1);
+                    assert(b.operators.size() == 1);
+
+                    return 2. * expectation_value(a.operators.front(), b.operators.front());
+                }
+
+                Float expectation_value_1_3(const Term& a, const Term& b) const
+                {
+                    assert(a.operators.size() == 1);
+                    assert(b.operators.size() == 3);
+
+                    Float ret = expectation_value(b.operators.back(), b.operators[1]);
+                    if(b.operators[1] == b.operators[2]) {
+                        ret -= 0.5;
+                    }
+                    ret *= expectation_value(a.operators.front(), b.operators.front());
+                    return 4 * ret;
+                }
+
+                Float expectation_value_3_3(const Term& a, const Term& b) const
+                {
+                    assert(a.operators.size() == 3);
+                    assert(b.operators.size() == 3);
+
+                    const auto& ops_a = a.operators;
+                    const auto& ops_b = b.operators;
+
+                    Float ev_6 = 8 * expectation_value(ops_a[0], ops_b[0])
+                                 * expectation_value(ops_a[1], ops_b[1])
+                                 * expectation_value(ops_b[1], ops_b[2]);
+                    ev_6 += 8 * expectation_value(ops_a[0], ops_b[0])
+                            * expectation_value(ops_a[1], ops_b[1])
+                            * (((ops_a[2].index1 == ops_b[2].index1) ? 1. : 0.)
+                               - expectation_value(ops_b[2], ops_a[2]));
+
+                    Float ev_4 = 0.;
+                    if(ops_b[1].index1 == ops_b[2].index1) {
+                        ev_4 -= 4 * expectation_value(ops_a[0], ops_b[0])
+                                * expectation_value(ops_a[1], ops_a[2]);
+                    }
+                    if(ops_a[1].index1 == ops_a[2].index1) {
+                        ev_4 -= 4 * expectation_value(ops_a[0], ops_b[0])
+                                * expectation_value(ops_b[1], ops_b[2]);
+                    }
+
+                    Float ev_2 = 0.;
+                    if((ops_a[1].index1 == ops_a[2].index1)
+                       && (ops_b[1].index1 == ops_b[2].index1)) {
+                        ev_2 += 2 * expectation_value(ops_a[0], ops_b[0]);
+                    }
+
+                    return ev_6 + ev_4 + ev_2;
+                }
+
                 template <typename Vector>
                 Float operator()(const Basis& basis, const Vector& vector) const
                 {
@@ -76,79 +139,34 @@ namespace ieompp
                     for(auto i = 0ul; i < N; ++i) {
                         const auto thread = omp_get_thread_num();
                         for(auto j = 0ul; j < i; ++j) {
-                            results[thread] +=
-                                expectation_value(basis[i].operators[0], basis[j].operators[0])
-                                * types::add_conjugate_products(vector[i], vector[j]);
+                            results[thread] += expectation_value_1_1(basis[i], basis[j])
+                                               * types::add_conjugate_products(vector[i], vector[j])
+                                               / 2.;
                         }
                         results[thread] +=
-                            std::norm(vector[i])
-                            * expectation_value(basis[i].operators[0], basis[i].operators[0]);
+                            std::norm(vector[i]) * expectation_value_1_1(basis[i], basis[i]) / 2.;
                     }
 
 #pragma omp parallel for schedule(dynamic, 1)
                     for(auto i = 0ul; i < N; ++i) {
                         const auto thread = omp_get_thread_num();
-                        const auto& op_a  = basis[i].operators[0];
                         for(auto j = N; j < basis_size; ++j) {
-                            const auto& ops_b = basis[j].operators;
-
-                            if(ops_b[1].index1 == ops_b[2].index1) {
-                                results[thread] +=
-                                    2 * expectation_value(op_a, ops_b[0])
-                                    * (expectation_value(ops_b[2], ops_b[1]) - .5)
-                                    * types::add_conjugate_products(vector[i], vector[j]);
-                            } else {
-                                results[thread] +=
-                                    2 * expectation_value(op_a, ops_b[0])
-                                    * expectation_value(ops_b[2], ops_b[1])
-                                    * types::add_conjugate_products(vector[i], vector[j]);
-                            }
+                            results[thread] += expectation_value_1_3(basis[i], basis[j])
+                                               * types::add_conjugate_products(vector[i], vector[j])
+                                               / 2.;
                         }
                     }
 
 #pragma omp parallel for schedule(dynamic, 1)
                     for(auto i = N; i < basis_size; ++i) {
-                        const auto thread    = omp_get_thread_num();
-                        const auto& ops_a    = basis[i].operators;
-                        const bool a_special = (ops_a[1].index1 == ops_a[2].index1);
+                        const auto thread = omp_get_thread_num();
                         for(auto j = N; j < i; ++j) {
-                            const auto& ops_b    = basis[j].operators;
-                            const bool b_special = (ops_b[1].index1 == ops_b[2].index1);
-                            const auto summand1  = 4 * expectation_value(ops_a[0], ops_b[0])
-                                                  * expectation_value(ops_a[1], ops_a[2])
-                                                  * expectation_value(ops_b[2], ops_b[1]);
-                            const auto summand2 =
-                                4 * expectation_value(ops_a[0], ops_b[0])
-                                * expectation_value(ops_a[1], ops_b[1])
-                                * (((ops_a[2].index1 == ops_b[2].index1) ? 1. : 0.)
-                                   - expectation_value(ops_b[2], ops_a[2]));
-                            const auto summand3 = a_special
-                                                      ? -2 * expectation_value(ops_a[0], ops_b[0])
-                                                            * expectation_value(ops_b[1], ops_b[2])
-                                                      : 0.;
-                            const auto summand4 = b_special
-                                                      ? -2 * expectation_value(ops_a[0], ops_b[0])
-                                                            * expectation_value(ops_a[1], ops_a[2])
-                                                      : 0.;
-                            const auto summand5 = (a_special && b_special)
-                                                      ? expectation_value(ops_a[0], ops_b[0])
-                                                      : 0.;
-                            results[thread] +=
-                                (summand1 + summand2 + summand3 + summand4 + summand5)
-                                * types::add_conjugate_products(vector[i], vector[j]);
+                            results[thread] += expectation_value_3_3(basis[i], basis[j])
+                                               * types::add_conjugate_products(vector[i], vector[j])
+                                               / 2.;
                         }
-                        const auto summand1 = 4 * expectation_value(ops_a[0], ops_a[0])
-                                              * expectation_value(ops_a[1], ops_a[2])
-                                              * expectation_value(ops_a[2], ops_a[1]);
-                        const auto summand2 = 4 * expectation_value(ops_a[0], ops_a[0])
-                                              * expectation_value(ops_a[1], ops_a[1])
-                                              * (1 - expectation_value(ops_a[2], ops_a[2]));
-                        const auto summand3 = a_special
-                                                  ? -4 * expectation_value(ops_a[0], ops_a[0])
-                                                            * expectation_value(ops_a[1], ops_a[2])
-                                                        + expectation_value(ops_a[0], ops_a[0])
-                                                  : 0.;
-                        results[thread] += (summand1 + summand2 + summand3) * std::norm(vector[i]);
+                        results[thread] +=
+                            expectation_value_3_3(basis[i], basis[i]) * std::norm(vector[i]) / 2.;
                     }
 
                     return std::accumulate(results.begin(), results.end(), 0.);
