cacad17fb247f992d0f1dc6464ba868c52b8c7ea
Fabian Köhler <fkoehler1024@googlemail.com>
2016-11-27 03:17:13 +0100

    add program to check that Fermi jump remains 1 when using Basis1

--- include/ieompp/models/hubbard_real_space/fermi_jump.hpp
+++ include/ieompp/models/hubbard_real_space/fermi_jump.hpp
@@ -25,6 +25,66 @@ namespace ieompp
             };
 
             template <typename FloatT, typename MonomialT>
+            class FermiJump1D<FloatT, Basis1Operator<MonomialT>>
+            {
+            public:
+                using Float    = FloatT;
+                using Monomial = MonomialT;
+                using Operator = typename Monomial::Operator;
+                using Basis    = Basis1Operator<Monomial>;
+                using Index    = typename Operator::Index1;
+                using Complex  = std::complex<Float>;
+                using ExpectationValueFunction =
+                    std::function<Float(const Operator&, const Operator&)>;
+
+            private:
+                std::vector<Complex> _fourier_coefficients;
+                const Index _N;
+                const ExpectationValueFunction _expectation_value;
+
+            public:
+                template <typename Lattice>
+                FermiJump1D(const Lattice& lattice, ExpectationValueFunction expectation_value)
+                    : _N(lattice.num()), _expectation_value(std::move(expectation_value))
+                {
+                    static const auto k_F = HalfPi<double>::value;
+
+                    _fourier_coefficients.reserve(_N);
+
+#pragma omp parallel for
+                    for(Index i = 0; i < _N; ++i) {
+                        const auto prod          = k_F * lattice[i];
+                        _fourier_coefficients[i] = Complex(std::cos(prod), std::sin(prod));
+                    }
+                }
+
+                template <typename Vector>
+                const Float operator()(const Basis& basis, const Vector& h) const
+                {
+                    static_cast<void>(basis);
+
+                    std::vector<Complex> results(omp_get_max_threads(), 0.);
+#pragma omp parallel for
+                    for(Index i = 0; i < _N; ++i) {
+                        results[omp_get_thread_num()] += _fourier_coefficients[i] * h[i];
+                    }
+
+                    return std::norm(
+                        std::accumulate(results.begin(), results.end(), Complex(0., 0.)));
+                }
+
+                const Complex& fourier_coefficient(const Index& index) const
+                {
+                    return _fourier_coefficients[index];
+                }
+
+                const Float expectation_value(const Operator& a, const Operator& b)
+                {
+                    return _expectation_value(a, b);
+                }
+            };
+
+            template <typename FloatT, typename MonomialT>
             class FermiJump1D<FloatT, Basis3Operator<MonomialT>>
             {
             public:
