33b7241cfe50e5f9f28d3a4e7713279eaf55e20b
Fabian Köhler <fkoehler1024@googlemail.com>
2016-12-11 02:15:39 +0100

    fix NonVanishingExpectationValues to account for ONB

--- include/ieompp/models/hubbard_momentum_space/non_vanishing_expectation_values.hpp
+++ include/ieompp/models/hubbard_momentum_space/non_vanishing_expectation_values.hpp
@@ -14,17 +14,27 @@ namespace ieompp
     {
         namespace hubbard_momentum_space
         {
-            template <typename IndexT>
-            class NonVanishingExpectationValues : public std::vector<std::pair<IndexT, IndexT>>
+            template <typename IndexT, typename FloatT>
+            struct NonVanishingExpectationValue {
+                using Index = IndexT;
+                using Float = FloatT;
+
+                const Index left_index, right_index;
+                const Float value;
+            };
+
+            template <typename IndexT, typename FloatT>
+            class NonVanishingExpectationValues
+                : public std::vector<NonVanishingExpectationValue<IndexT, FloatT>>
             {
             public:
-                using Index = IndexT;
+                using Contribution = NonVanishingExpectationValue<IndexT, FloatT>;
 
                 template <typename Monomial, typename Dispersion>
                 NonVanishingExpectationValues(const Basis3Operator<Monomial>& basis,
                                               const Basis3Operator<Monomial>& conjugate_basis,
                                               const Dispersion& dispersion,
-                                              const typename Dispersion::Float& fermi_energy = 0.)
+                                              const typename Contribution::Float& fermi_energy = 0.)
                 {
                     const auto basis_size = basis.size();
                     assert(basis.size() == conjugate_basis.size());
@@ -32,46 +42,118 @@ namespace ieompp
                     using State      = ExcitedFermiSea<Monomial>;
                     using BasisIndex = typename Basis3Operator<Monomial>::BasisIndex;
 
+                    // calculate b_i^† |FS>
                     std::vector<State> states(basis_size);
-
-                    // apply conjugate basis monomials to fermi_sea
-                    // -> calculate b_i^† |FS>
                     for(BasisIndex i = 0; i < basis_size; ++i) {
                         states[i].apply_monomial(conjugate_basis[i], dispersion, fermi_energy);
                     }
 
-                    // apply basis monomials and check for non-vanishing combinations
-                    for(BasisIndex i = 0; i < basis_size; ++i) {
-                        // skip state if it already vanishes
-                        if(states[i].vanishes) {
-                            continue;
+                    // check if b_0 b_0^† |FS> vanishes
+                    auto state        = states[0];
+                    bool n_q_vanishes = true;
+                    state.apply_monomial(basis[0], dispersion, fermi_energy);
+                    if(state.is_initial_fermi_sea()) {
+                        this->emplace_back(0, 0, 0.5);
+                        n_q_vanishes = false;
+                    }
+
+                    // check if b_i b_0^† |FS> vanishes
+                    for(BasisIndex i = 1; i < basis_size; ++i) {
+                        bool vanishes                      = true;
+                        typename Contribution::Float value = 0.;
+
+                        state = states[0];
+                        state.apply_monomial(basis[i], dispersion, fermi_energy);
+                        if(state.is_initial_fermi_sea()) {
+                            value    = 0.25;
+                            vanishes = false;
                         }
 
-                        for(BasisIndex j = 0; j < basis_size; ++j) {
-                            // get a working copy of state b_i^† |FS>
-                            State state = states[i];
+                        const bool kronecker = (basis[i][1].index1 == basis[i][2].index1);
+                        if(kronecker && !n_q_vanishes) {
+                            value += 0.5;
+                            vanishes = false;
+                        }
 
-                            // calculate b_j b_i^† |FS>
-                            state.apply_monomial(basis[j], dispersion, fermi_energy);
+                        if(!vanishes) this->emplace_back(i, 0, value);
+                    }
 
-                            // check if state is fermi sea
+                    // check if b_0 b_i^† |FS> vanishes
+                    for(BasisIndex i = 1; i < basis_size; ++i) {
+                        bool vanishes                      = true;
+                        typename Contribution::Float value = 0.;
+
+                        state = states[i];
+                        state.apply_monomial(basis[0], dispersion, fermi_energy);
+                        if(state.is_initial_fermi_sea()) {
+                            value    = 0.25;
+                            vanishes = false;
+                        }
+
+                        const bool kronecker =
+                            (conjugate_basis[i][1].index1 == conjugate_basis[i][2].index1);
+                        if(kronecker && !n_q_vanishes) {
+                            value += 0.5;
+                            vanishes = false;
+                        }
+
+                        if(!vanishes) this->emplace_back(0, i, value);
+                    }
+
+                    // check if b_i b_j^† |FS> vanishes
+                    for(BasisIndex i = 1; i < basis_size; ++i) {
+                        for(BasisIndex j = 1; j < basis_size; ++j) {
+                            bool vanishes                      = true;
+                            typename Contribution::Float value = 0.;
+
+                            state = states[j];
+                            state.apply_monomial(basis[i], dispersion, fermi_energy);
                             if(state.is_initial_fermi_sea()) {
-                                // <FS| b_j b_i^† |FS> = 1
-                                // store index combination j, i
-                                this->emplace_back(std::pair<Index, Index>{j, i});
+                                value    = 0.125;
+                                vanishes = false;
                             }
+
+                            const bool kronecker1 = (basis[i][1].index1 == basis[i][2].index1);
+                            const bool kronecker2 =
+                                (conjugate_basis[i][1].index1 == conjugate_basis[i][2].index1);
+
+                            if(kronecker1) {
+                                auto state = states[j];
+                                state.apply_monomial(basis[0], dispersion, fermi_energy);
+                                if(state.is_initial_fermi_sea()) {
+                                    value += 0.25;
+                                    vanishes = false;
+                                }
+                            }
+
+                            if(kronecker2) {
+                                auto state = states[0];
+                                state.apply_monomial(basis[i], dispersion, fermi_energy);
+                                if(state.is_initial_fermi_sea()) {
+                                    value += 0.25;
+                                    vanishes = false;
+                                }
+                            }
+
+                            if(kronecker1 && kronecker2 && !n_q_vanishes) {
+                                value += 0.5;
+                                vanishes = false;
+                            }
+
+                            if(!vanishes) this->emplace_back(i, j, value);
                         }
                     }
                 }
 
                 void sort()
                 {
-                    std::sort(this->begin(), this->end(), [](const std::pair<Index, Index>& a,
-                                                             const std::pair<Index, Index>& b) {
-                        if(a.first < b.first) return true;
-                        if(a.first == b.first) return (a.second < b.second);
-                        return false;
-                    });
+                    std::sort(this->begin(), this->end(),
+                              [](const Contribution& a, const Contribution& b) {
+                                  if(a.left_index < b.left_index) return true;
+                                  if(a.left_index == b.left_index)
+                                      return (a.right_index < b.right_index);
+                                  return false;
+                              });
                 }
             };
         } // namespace hubbard_momentum_space
